8a9e6c4ce011d0b46cc7c0d4b147b3801a74e08f
Savitha <savithapremkumar@gmail.com>
2020-10-13 13:22:49 +1300

    Removed app.css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import Header from "./components/Header/index";
+import Home from "./views/Home/index";
+import Photos from "./views/Photos/index";
+import "./styles/main.scss";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { SiteHeading } from "./constants/messages";
 
 function App() {
   return (
-    <div data-test="appComponent" className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div data-test="appComponent" className="App">
+        <Header heading={SiteHeading} />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route
+            path="/albums/:albumId/photos"
+            render={(props) => {
+              return <Photos {...props}></Photos>;
+            }}
+          />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
